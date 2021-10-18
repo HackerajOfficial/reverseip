7d83d38bbaad045a2c1e6b38e10aab744e592911
HackerajOfficial <hackeraj.np@hotmail.com>
2021-10-19 03:40:36 +0545

    Add argparse

--- reverse_ip.py
+++ reverse_ip.py
@@ -1,11 +1,15 @@
 import requests
+import argparse
+
+parser = argparse.ArgumentParser(description='A Python Based Reverse IP Script To Find The Domains On The Server. Coded By Hackeraj')
+parser.add_argument('site', type=str, help='Site Link: raaz.info.np')
+args = parser.parse_args()
 
 def reverseiplookupsUsingHackerTarget(site):
     URL = "https://api.hackertarget.com/reverseiplookup/?q="
     combo = "{url}{website}".format(url=URL, website=site)
     r = requests.get(combo)
     print(r.text)
-# #print(dir(r)) #To know the all we can access
+# #print(dir(r)) #To know the all we can access 
 
-site = input("URL:")
-reverseiplookupsUsingHackerTarget(site)
+reverseiplookupsUsingHackerTarget(args.site)
